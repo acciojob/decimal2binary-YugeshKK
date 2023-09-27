de2a52b2542b4b3f7dbd4301bf8c2e60ecd56c20
YugeshKK <95625392+YugeshKK@users.noreply.github.com>
2023-09-27 11:06:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+  let bin=0;
+	let i=, rem=1;
+	while(num!=0){
+		rem=num%2;
+		num=parseInt(num/2);
+		bin= bin+rem*i;
+		i=i*10;
+	}
+	return bin;
+	
 }
 
 window.decimalToBinary = decimalToBinary;
